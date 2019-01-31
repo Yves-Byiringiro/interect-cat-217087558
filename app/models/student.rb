d088269a7876dc42781bd9134ab90966002f4561
Yves <45223489+Yves-Byiringiro@users.noreply.github.com>
2019-01-31 12:34:46 +0000

    first commit

---
+++ app/models/student.rb
@@ -0,0 +1,7 @@
+class Student < ApplicationRecord
+    validates :regno , presence :true
+    validates :name , presence :true
+    validates :telephone , presence :true
+    validates :address , presence :true
+    
+end
